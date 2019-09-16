e13654645f0ca6e07a8667d8cc412d6b2f0157dc
harvanir <harvanjns@gmail.com>
2019-09-16 17:51:26 +0700

    Customize repository base class for performance perspective

--- src/main/java/org/harvanir/gradle/gradledemo/configuration/JpaConfiguration.java
+++ src/main/java/org/harvanir/gradle/gradledemo/configuration/JpaConfiguration.java
@@ -1,13 +1,13 @@
 package org.harvanir.gradle.gradledemo.configuration;
 
-import org.harvanir.gradle.gradledemo.repository.support.QueryDslJpaEnhancedRepositoryImpl;
+import org.harvanir.gradle.gradledemo.repository.support.JpaEnhancedRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /** @author Harvan Irsyadi */
 @EnableJpaRepositories(
-    repositoryBaseClass = QueryDslJpaEnhancedRepositoryImpl.class,
+    repositoryBaseClass = JpaEnhancedRepository.class,
     basePackages = "org.harvanir.gradle.gradledemo.repository")
 @EnableJpaAuditing
 @Configuration
