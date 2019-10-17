1d51cfafc50885efcf2998aa639923b030eda98f
harvanir <harvanir@gmail.com>
2019-10-17 16:38:24 +0700

    Polishing

--- src/main/java/org/harvanir/gradle/gradledemo/entity/request/order/OrderCreateRequest.java
+++ src/main/java/org/harvanir/gradle/gradledemo/entity/request/order/OrderCreateRequest.java
@@ -2,7 +2,12 @@ package org.harvanir.gradle.gradledemo.entity.request.order;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Set;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /** @author Harvan Irsyadi */
 @Builder
